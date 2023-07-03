5d9d59f782d194e97ce5deb719300f9a1b768218
Natan <112595785+wildBlueTurtle@users.noreply.github.com>
2023-07-02 22:59:55 -0300

    Update gestionCRUD.js

--- Aplicaciones/Libreria/static/js/gestionCRUD.js
+++ Aplicaciones/Libreria/static/js/gestionCRUD.js
@@ -3,14 +3,21 @@
   /*
   summary: funcion autoejecutable que agrega un evento (mensaje de confirmación) de click a todos los elementos con clase "btnEliminacion"
   */
-  const btnEliminacion = document.querySelectorAll(".btnEliminacion"); // Creamos una variable constante que almacena una lista de elementos con la clase .btnEliminación
-
-  //A continuación para cada elemento de la lista aplicamos un evento de escucha.
+  
+  // Seleccionamos todos los elementos que tengan la clase "btnEliminacion" y los almacenamos en la constante "btnEliminacion".
+  const btnEliminacion = document.querySelectorAll(".btnEliminacion"); 
+  
+  // Usamos el método "forEach" para iterar sobre cada botón con la clase "btnEliminacion".
   btnEliminacion.forEach((btn) => {
+    // Agregamos un evento "click" a cada botón seleccionado.
     btn.addEventListener("click", (eventClick) => {
+      // Cuando se hace clic en el botón, se muestra un cuadro de diálogo de confirmación con el mensaje "¿Seguro que desea eliminar el usuario?".
       const confirmation = confirm("¿Seguro que desea eliminar el usuario?");
+      
+      // Si el usuario hace clic en "Aceptar" (es decir, confirma la eliminación), no hacemos nada y el proceso de eliminación continuará normalmente.
+      // Si el usuario hace clic en "Cancelar" (es decir, niega la eliminación), ejecutamos "eventClick.preventDefault()" para evitar que el evento de clic del botón se propague y realice la eliminación.
       if (!confirmation) { 
-        eventClick.preventDefault(); //Si la confirmación devuelve false se cancelará el evento click
+        eventClick.preventDefault();
       }
     });
   });
